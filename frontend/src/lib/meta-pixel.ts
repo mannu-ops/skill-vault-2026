3aab885beeb3f62be29e6c3eac390d88c4c0793d
smanpree20@gmail.com <temp83725@gmail.com>
2026-08-25 04:52:20 +0530

    Prevent duplicate fbq init registration call

--- frontend/src/lib/meta-pixel.ts
+++ frontend/src/lib/meta-pixel.ts
@@ -118,33 +118,36 @@ export function initMetaPixel(pixelId: string = DEFAULT_PIXEL_ID, userData?: Use
 
   window.__META_PIXEL_ID__ = targetPixelId;
 
-  if (window.__META_PIXEL_INITIALIZED__ && (!userData || Object.keys(userData).length === 0)) {
+  // If window.fbq already exists (e.g. initialized in index.html snippet), avoid duplicate fbq('init') call
+  if (window.fbq) {
+    window.__META_PIXEL_INITIALIZED__ = true;
+    if (userData && Object.keys(userData).length > 0) {
+      setUserProperties(userData);
+    }
     return true;
   }
 
   // Inject Meta Pixel script if not present
-  if (!window.fbq) {
-    /* eslint-disable */
-    const n: any = (window.fbq = function () {
-      n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
-    });
-    if (!window._fbq) window._fbq = n;
-    n.push = n;
-    n.loaded = true;
-    n.version = '2.0';
-    n.queue = [];
+  /* eslint-disable */
+  const n: any = (window.fbq = function () {
+    n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+  });
+  if (!window._fbq) window._fbq = n;
+  n.push = n;
+  n.loaded = true;
+  n.version = '2.0';
+  n.queue = [];
 
-    const t = document.createElement('script');
-    t.async = true;
-    t.src = 'https://connect.facebook.net/en_US/fbevents.js';
-    const s = document.getElementsByTagName('script')[0];
-    if (s && s.parentNode) {
-      s.parentNode.insertBefore(t, s);
-    } else {
-      document.head.appendChild(t);
-    }
-    /* eslint-enable */
+  const t = document.createElement('script');
+  t.async = true;
+  t.src = 'https://connect.facebook.net/en_US/fbevents.js';
+  const s = document.getElementsByTagName('script')[0];
+  if (s && s.parentNode) {
+    s.parentNode.insertBefore(t, s);
+  } else {
+    document.head.appendChild(t);
   }
+  /* eslint-enable */
 
   const advancedMatchingData = formatUserData(userData);
 
